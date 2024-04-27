c8d424f504a136ea9923d6eb638276afe0c29235
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-04-27 21:57:51 +0600

    Add files via upload

---
+++ STL.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;  //vector is continuous memory block like array//
+/*void printvec(vector<int>&v){
+    cout<<"size:"<<v.size()<<endl;
+for(int i=0;i<v.size();i++){
+
+    cout<<v[i]<<" ";
+}
+v.push_back(2);
+cout<<endl;
+}
+
+int main(){
+vector<int>v;
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+    int x;
+    cin>>x;
+    printvec(v);
+    v.push_back(x);
+}
+printvec(v);
+vector<int>v(10,3);
+v.push_back(7);
+v.pop_back();
+printvec(v);
+vector<int>&v2=v;//vector copy ....array r khetre ebhabe korle array er pointer copy hoto//.....but time complexity O(n)..tai functiion a ref pass korbo
+v2.push_back(5);
+printvec(v);
+printvec(v2);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//Vector of Pair///
+
+
+
+void printvec(vector<pair<int,int>>&v){
+cout<<"size:"<<v.size()<<endl;
+for(int i=0;i<v.size();i++){
+    cout<<v[i].first<<" "<<v[i].second<<" ";
+}
+cout<<endl;
+}
+
+int main(){
+
+vector<pair<int,int>>v;
+printvec(v);
+int n;
+cin>>n;
+for(int i=0;i<n;i++){
+    int x,y;
+    cin>>x>>y;
+    v.push_back({x,y});
+}
+ printvec(v) ;
+}*/
+
+
+
+
+
+
+
+
+
+
+//Vector of Array//
+
+
+
+
+
+
+
+
+
+
