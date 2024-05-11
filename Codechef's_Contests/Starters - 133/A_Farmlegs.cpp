8ab6e1db7ec0f0182b1ade4797d7a904b16d85a6
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-05-11 12:04:52 +0600

    Update A_Farmlegs.cpp

--- Codechef's_Contests/Starters - 133/A_Farmlegs.cpp
+++ Codechef's_Contests/Starters - 133/A_Farmlegs.cpp
@@ -1,1 +1,32 @@
+Problem Url :https://www.codechef.com/START133D/problems/FARMLEGS
+
+#include<bits/stdc++.h>
+using namespace std;
+#define nn '\n'
+#define ll long long int
+void solve() {
+   int n;
+   cin>>n;
+   if(n==2 || n==4)cout<<"1"<<nn;
+   else{
+      if(n%4==0){
+         cout<<n/4<<nn;
+      }
+      else{
+         cout<<(n/4)+1<<nn;
+      }
+   }   
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int test;
+    cin>>test;
+    while(test--){
+    solve();
+   }
+return 0;
+}
+
 
