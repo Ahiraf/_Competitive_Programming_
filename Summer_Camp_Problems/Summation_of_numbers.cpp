0943b33eb0274d9a7ac098afe01f79bf686e264a
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-05-21 08:15:04 +0600

    Create Summation_of_numbers.cpp

---
+++ Summer_Camp_Problems/Summation_of_numbers.cpp
@@ -0,0 +1,27 @@
+//Given a number n.Print the summation of the numbers that between 1 and n;
+
+#include<bits/stdc++.h>
+using namespace std;
+#define nn '\n'
+#define ll long long int
+const long long int Max=1e9+7;
+void solve() {
+    ll n;
+    cin>>n;
+    cout<<n*(n+1)/2<<nn;
+    
+      
+      
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll test; cin>>test;while(test--)
+    solve();
+   
+return 0;
+}
+
+
+
