a62f0a8242b031164d9d08db2f6c478f411864db
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-05-31 21:12:08 +0600

    Create LCM_of_2Numbers.cpp

---
+++ _GCD_&_LCM_/LCM_of_2Numbers.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn '\n'
+#define ll long long int
+#define ld long double
+const long long int Max=1e9+7;
+long double PI=acos(-1);
+ //LCM = Lowest Common Multiple
+ //Using Ecludian Algorithm ...There are 2 types in this Algorithm : 1) Basic and 2)Extended
+ //The Basic is given below:
+
+int gcd(int a,int b){
+    if(a>b)swap(a,b);
+    else{
+        if(a==0) return b; 
+        return gcd(b%a,a);
+    }
+}
+int lcm(int a,int b){  //lo-sha-gu
+    return (a*b)/gcd(a,b);
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+   int a,b;
+   cin>>a>>b;
+   cout<<lcm(a,b)<<nn;  //time complexity = O(log(min(a,b)))
+return 0;
+}
+ 
+ 
