6e278a5a9f2dfe5bf4cb4285e5e4cbb1f109b2c1
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-10-10 17:20:34 +0600

    Create 35_MagicNumbers.cpp

---
+++ CP_800_to_1400_Solutions/35_MagicNumbers.cpp
@@ -0,0 +1,38 @@
+//Problem Url : https://codeforces.com/contest/320/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define nn "\n"
+#define pb push_back
+#define yes() cout<<"YES"<<"\n"
+#define no() cout << "NO" << "\n"
+#define all(x) (x).begin(),(x).end()
+const int MN = -1e9;
+const int N=1e5;
+
+bool is_magical(string s){
+    for(int i=0;i<(int)s.size();i++)
+    {
+        if(s[i]!='1' && s[i]!='4')return false;
+        else if(s[0]=='4')return false;
+        else if(s.find("444") != s.npos)return false;
+    }
+    return true;
+}
+void kodiko(){
+   string s;
+   cin>>s;
+   if(is_magical(s))cout<<"YES\n";
+   else cout<<"NO\n"; 
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //int test; cin >> test; while (test--)
+    kodiko();
+    return 0;
+}
+ 
