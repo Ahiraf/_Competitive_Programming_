daa392b99e00e379af755ba92a5d6b7072303ab1
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2025-01-01 08:45:52 +0600

    Update 05_Fermat's-Theorem.cpp

--- Number_Theory/05_Fermat's-Theorem.cpp
+++ Number_Theory/05_Fermat's-Theorem.cpp
@@ -6,19 +6,33 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-        int c;
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
         for (int i = 2; i * i <= c; i++) {
             int count = 0;
             while (c % i == 0) {
                 count++;
                 c /= i;
             }
-           if (i % 4 == 3 && count % 2 != 0){
-             cout<<"C can be expressed as a^2 + b^2"<<"\n";
-             return 0;
-           }
+            if (i % 4 == 3 && count % 2 != 0) return false;
         }
-      cout<<"C can not be expressed as a^2 + b^2<<"\n"; 
+        return c % 4 != 3;
     }
+}; // Time complexity : O(sqrt(c))
+
+/*
+Why Check c % 4 != 3?
+If c % 4 == 3:c is a prime number congruent to 3 modulo 4, and its power is implicitly 1 (odd).This violates 
+the sum-of-squares condition.In this case, the function should return false.
+
+If c%4!=3: The remaining c is either ≡ 1 mod 4 or 𝑐 = 1.Both cases are valid under the sum-of-squares condition.
+The function can safely return true.
+
+
+Example 1:  c=10  , Prime factorisation: 10 = 2^1 × 5^1. Neither 2 nor 5 is of the form 4k+3.
+Remaining c=1 after loop (no problematic primes).
+Final return: 1%4!=3 → true. Finally, c = 1^2 + 3^2 .
+
+ */
